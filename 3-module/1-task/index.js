56e33677781086f8cd5b83e51dd94509dfd55be3
Dryupin <sifon13482@gmail.com>
2024-05-03 13:18:09 +0300

    module3 task1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,17 @@
+const { runner } = require("karma")
+
 function namify(users) {
-  // ваш код...
+  let userNames = users.map(item => item.name);
+  return userNames;
 }
+/*       let userNames = (users.map( (item, index) => item.name ));
+      return userNames; 
+
+
+      `${item.name}`
+      
+      
+      
+      
+      
+      */
