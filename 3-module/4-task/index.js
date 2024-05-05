80d1d09d5952e43b47a80147283f1e7372c265a3
Dryupin <sifon13482@gmail.com>
2024-05-05 19:23:01 +0300

    3-module 4,5 task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,9 @@
 function showSalary(users, age) {
-  // ваш код...
+  let sort = users
+    .filter(item => item.age <= age)
+    .map(item => `${item.name}, ${item.balance}`)
+    .join("\n")
+    ;
+
+  return sort
 }
