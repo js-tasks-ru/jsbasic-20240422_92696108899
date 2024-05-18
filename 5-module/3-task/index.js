d005d2768a2e83d4d1304a3f4de18feef5654d1b
Mikhail <sifon13482@gmail.com>
2024-05-18 11:18:48 +0300

    5-module 3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
-  // ваш код...
+
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let carouselWidth = carouselInner.offsetWidth;
+
+  arrowLeft.style.display = 'none';
+
+  arrowRight.addEventListener('click', () => {
+    arrowLeft.style.display = '';
+    let currentCarouselWidth = new WebKitCSSMatrix(
+      getComputedStyle(carouselInner).transform
+    );
+
+    if (!Math.abs(currentCarouselWidth.m41 % carouselWidth)) {
+      carouselInner.style.transform = `translateX(-${
+        carouselWidth - currentCarouselWidth.m41
+      }px)`;
+      if (currentCarouselWidth.m41 < -988) {
+        arrowRight.style.display = 'none'
+      };
+    }
+  });
+
+  arrowLeft.addEventListener('click', () => {
+    arrowRight.style.display = '';
+    let currentCarouselWidth = new WebKitCSSMatrix(
+      getComputedStyle(carouselInner).transform
+    );
+
+    if (!Math.abs(currentCarouselWidth.m41 % carouselWidth)) {
+      carouselInner.style.transform = `translateX(${
+        carouselWidth + currentCarouselWidth.m41
+      }px)`;
+      if (currentCarouselWidth.m41 >= -988) {
+        arrowLeft.style.display = 'none'
+      };
+    }
+  });
 }
+
