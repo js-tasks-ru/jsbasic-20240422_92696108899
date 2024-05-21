1bb4d1bdcf45ead68314dc606a3ec716fcbdbf64
Dryupin <sifon13482@gmail.com>
2024-05-21 15:27:36 +0300

    6-module 1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -10,9 +10,47 @@
  *          salary: '1000',
  *          city: 'Petrozavodsk'
  *      }
- *
+ * 
  */
+
+// let rows = [
+//   {
+//       name: 'Вася',
+//       age: 25,
+//       salary: 1000,
+//       city: 'Самара'
+//   },
+//   {
+//       name: 'Петя',
+//       age: 30,
+//       salary: 1500,
+//       city: 'Москва'
+//   }
+// ];
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.add();
+    this.remove();
+  }
+  add() {
+    this.elem = document.createElement('TABLE')
+    this.elem.innerHTML = this.rows
+      .map(({ name, age, salary, city }) =>
+        `<tr>
+          <td>${name}</td><td>${age}</td><td>${salary}</td><td>${city}</td><td><button>X</button></td>
+        </tr>`)
+      .join('');
+
+    this.user
+  }
+
+  remove() {
+    this.elem.addEventListener('click', ({ target }) => {
+      if (target.closest('button')) {
+        let del = target.parentNode.querySelector('button').parentNode.parentNode;
+        del.remove();
+      }
+    });
   }
 }
