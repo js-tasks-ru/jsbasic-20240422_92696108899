973d8498f4d4b6fa8e630d18f79f09a3b480a42b
Mikhail <sifon13482@gmail.com>
2024-06-01 23:30:01 +0300

    7-4 Drag-and-Drop

--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -28,49 +28,61 @@ export default class StepSlider {
 
     this.elem.querySelector('.slider__value').textContent = this.value;
   };
+  // У вас не было обработчика на событие click-а
+  onClick = (event) => {
+    let newLeft = (event.clientX - this.elem.getBoundingClientRect().left) / this.elem.offsetWidth;
+
+    this.value = Math.round((this.steps - 1) * newLeft);
+    this.valuePercents = (this.value / (this.steps - 1)) * 100;
+    this.updateSliderStyle();
+    this.sendDataServer();
+  }
 
   move() {
     this.sliderThumb = this.elem.querySelector('.slider__thumb');
     this.sliderProgress = this.elem.querySelector('.slider__progress');
-    this.elem.ondragstart = () => false;
+    this.sliderThumb.ondragstart = () => false;
+    this.elem.onclick = this.onClick;
 
-    this.elem.addEventListener('pointerdown', (mousedownEvent) => {
+    this.sliderThumb.addEventListener('pointerdown', (mousedownEvent) => {
+      this.elem.classList.add('slider_dragging');
 
       if (mousedownEvent.target.closest('.slider__thumb')) {
         // 1) mousedown - подготовка к перемещению
-        this.shiftX = mousedownEvent.clientX;
         this.sliderThumb.style.position = 'absolute';
         this.sliderThumb.style.zIndex = 1000;
-        console.log('this.elem.getBoundingClientRect().left', this.elem.getBoundingClientRect().left)
 
         // 2) mousemove - передвигаем элемент на новые координаты меняя left/top
         this.onMousemove = (mouseMoveEvent) => {
-          let x = mouseMoveEvent.clientX - this.elem.getBoundingClientRect().left;
-          if (x >= 0 && x <= this.elem.offsetWidth) {
-            this.sliderThumb.style.left = x + 'px';
-            this.sliderProgress.style.width = x + 'px';
-          }
+
+          let x = (mouseMoveEvent.clientX - this.elem.getBoundingClientRect().left) / this.elem.offsetWidth;
+
+          if (x < 0) { x = 0; }
+          if (x > 1) { x = 1; }
+
+          this.sliderThumb.style.left = x * 100 + '%';
+          this.sliderProgress.style.width = x * 100 + '%';
         };
         this.elem.addEventListener('pointermove', this.onMousemove);
-
       } // END if (mousedownEvent.target.closest('.slider__thumb')
 
-      let onMouseUp = document.addEventListener('pointerup', (mouseUpEvent) => {
+      this.onMouseUp = document.addEventListener('pointerup', (mouseUpEvent) => {
+        this.elem.classList.remove('slider_dragging');
 
         let newLeft = (mouseUpEvent.clientX - this.elem.getBoundingClientRect().left) / this.elem.offsetWidth;
         if (newLeft > 1) newLeft = 1;
         if (newLeft < 0) newLeft = 0;
         this.value = Math.round((this.steps - 1) * newLeft);
         this.valuePercents = (this.value / (this.steps - 1)) * 100;
-
         if (mousedownEvent.target.closest('.slider__thumb')) {
           this.elem.removeEventListener('pointermove', this.onMousemove);
-          this.elem.removeEventListener('pointerup', onMouseUp);
-          onMouseUp = null
-        }
+          this.elem.removeEventListener('pointerup', this.onMouseUp);
+          document.removeEventListener('pointerup', this.onMouseUp);
+          this.onMouseUp = null;
 
-        this.updateSliderStyle();
-        this.sendDataServer();
+          this.updateSliderStyle();
+          this.sendDataServer();
+        }
       });
     })
   };
@@ -84,6 +96,7 @@ export default class StepSlider {
       this.elem.querySelector('.slider__step-active').classList.remove('slider__step-active');
     }
     this.elem.querySelector('.slider__steps').children[this.value].classList.add('slider__step-active');
+    // this.elem.removeEventListener('pointerup', this.onMouseUp);
   };
 
   sendDataServer() {
@@ -93,6 +106,4 @@ export default class StepSlider {
     }));
   };
 }
-
-
 
